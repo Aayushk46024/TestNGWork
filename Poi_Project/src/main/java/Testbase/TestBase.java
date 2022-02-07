d536e35f1e87e76e50b9562f81d1d16a62de1be3
Aayush Khandelwal <96468575+Aayushk46024@users.noreply.github.com>
2022-02-07 20:26:43 +0530

    Test webpage using POM framework and data driven framework

--- Poi_Project/src/main/java/Testbase/TestBase.java
+++ Poi_Project/src/main/java/Testbase/TestBase.java
@@ -20,10 +20,6 @@ public class TestBase {
     public static Properties prop;
     public static WebDriver driver;
 
-    Date currentdate = new Date();
-    //System.out.println(currentdate);
-    String screenshotfilename = currentdate.toString().replace(" ", "-").replace(":", "-");
-    //System.out.println(screenshotfilename);
 
     public TestBase()  {
         try {
@@ -42,9 +38,6 @@ public class TestBase {
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
         driver.get(prop.getProperty("url"));
-
-        File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(screenshotFile, new File(".//screenshot//"+ screenshotfilename +".png"));
 
     }
 
